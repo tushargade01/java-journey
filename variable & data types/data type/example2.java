b71c6bb9db719161c42474add41de600f22cd561
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-02-03 17:57:54 +0530

    Add files via upload

---
+++ variable & data types/data type/example2.java
@@ -0,0 +1,8 @@
+public class example2 {
+    public static void main(String[] args){
+        byte a = 1;
+        short b = 2;
+        System.out.println(a+b); //return int value
+        System.out.println("hello world!");
+    }   
+}

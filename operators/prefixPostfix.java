f9b2b8d6a8f6f7d3ddcb2b9f631ca8253b132146
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-02-03 23:54:06 +0530

    Add files via upload

---
+++ operators/prefixPostfix.java
@@ -0,0 +1,9 @@
+public class prefixPostfix {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        System.out.println(a++);
+        System.err.println(++a);
+        System.out.println(a);
+    }    
+}

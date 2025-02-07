bf6b6ed1ae982813c059a579b23b45f3c5f1006e
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-02-07 23:09:54 +0530

    Add files via upload

---
+++ function/noParameters.java
@@ -0,0 +1,9 @@
+public class noParameters {
+    public static void helloWrold() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        helloWrold();
+        helloWrold();
+    }
+}

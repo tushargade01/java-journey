a92c76e1546445618779106d48b69a288db81e1f
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-02-07 23:12:26 +0530

    Add files via upload

---
+++ function/addtion.java
@@ -0,0 +1,10 @@
+public class addtion {
+    public static int addition(int a, int b) {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int result = addition(5, 4);
+        System.out.println(result);
+        System.out.println(addition(3, 2));
+    }
+}

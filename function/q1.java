fce3a8bf55371fc4d6730691aa216bf41ba6c0a1
Tushar Gade <110712003+tushargade01@users.noreply.github.com>
2025-02-11 23:25:06 +0530

    Add files via upload

---
+++ function/q1.java
@@ -0,0 +1,9 @@
+public class q1{
+    static int avgSum(int a, int b, int c){
+        int avgSum = (a+b+c)/3;
+        return avgSum;
+    }
+    public static void main(String[] args) {
+        System.out.println(avgSum(2, 4, 5));
+    }
+}
